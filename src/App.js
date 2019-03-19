f7568b64dbdcf5c85b6a50b721434d4367eaf651
Ben Rugman <benrugman@yahoo.co.uk>
2019-03-19 17:04:40 +0000

    final logic solved application working

--- src/App.js
+++ src/App.js
@@ -2,55 +2,66 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from './components/Button';
 
+// using math.js as an alternative to native eval function
+const math = require('mathjs');
+
+
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      display: 0,
-      operatorDisplayed: false,
+      display: '0',
+      // this state allows control over how many decimal points can be entered
       decimalDisplayed: false
     }
   }
   
-  buttonPress = (e) => {
-    let key = e.target.value;
-    
-    if(key === 'AC'){
-      return this.clearCalculator();
-    }
-    if(key === '='){
-      return this.equateSum();
-    }     
-    if(key === '0'|'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'){
-      return this.displayNumber(e);
-    }
-    if(key === '&#x2b;' || '&minus;' || '&times;' || '&divide;'){
-      console.log(key);
-      return;
-    }
-    if(key === '.'){
-      this.setState({display: 0});
-    }
-  }
-  
-  
+  // clears the calculator display
   clearCalculator = () => {
-    console.log('clicked');
-    this.setState({display: 0});
+    this.setState({display: '0',
+                   decimalDisplayed: false});
   }
-
+  
+  // function controls how numbers are displayed if there is just a 0 in the calculator display
   displayNumber = (e) => {
     if(e.target.value === '0'){
-      this.setState({display: this.state.display === 0 ? 0 : this.state.display + e.target.value});
+      this.setState({display: this.state.display === '0' ? '0' : this.state.display + e.target.value});
     } else {
-      this.setState({display: this.state.display === 0 ? e.target.value : this.state.display + e.target.value});
+      this.setState({display: this.state.display === '0' ? e.target.value : this.state.display + e.target.value});
     }  
   } 
-      
 
+  // function to enter only one decimal place per number
+  displayDecimal = () => {
+    if(this.state.decimalDisplayed === false){
+      this.setState({display: this.state.display + '.',
+                     decimalDisplayed: true
+                    });
+    }
+  }
+
+  // function controls how operators can be used.  Only one between two numbers.
+  displayOperator = (e) => {
+    let operator = e.target.value;
+    // string manipulation to check the last digit in the display
+    let lastDigit = this.state.display.slice(-1);
+
+    // if the last entered digit was an operator then it replaces that operator with a new operator or if not then adds the operator to the display
+    if(lastDigit === '*' || lastDigit === '-' || lastDigit === '/' || lastDigit === '+'){
+      this.setState({display: this.state.display.slice(0, -1) + operator});
+    } else {
+      this.setState({display: this.state.display + operator});
+    }
+
+    // once an operator has been pressed then this allows the decimal key to become active again
+    this.setState({decimalDisplayed: false});
+  }
+      
+  // uses math.js to evaluate the equation
   equateSum = () => {
-    let sum = this.state.display;
-    console.log(sum);
+    let num = math.eval(this.state.display).toString();
+    this.setState({display: num, displayDecimal: true});
   }
 
   render() {
@@ -61,23 +72,23 @@ class App extends Component {
           <div id = 'display'>{this.state.display}</div>
         </div>
         <div id='buttons'>
-          <Button buttonPress = {this.buttonPress}idName='add' buttonName='&#x2b;' />
-          <Button buttonPress = {this.buttonPress}idName='subtract' buttonName='&minus;' />
-          <Button buttonPress = {this.buttonPress}idName='multiply' buttonName = '&times;' />
-          <Button buttonPress = {this.buttonPress}idName='divide' buttonName='&divide;' />
-          <Button buttonPress = {this.buttonPress}idName='one' buttonName='1' />
-          <Button buttonPress = {this.buttonPress}idName='two' buttonName='2' />
-          <Button buttonPress = {this.buttonPress}idName='three' buttonName='3' />
-          <Button buttonPress = {this.buttonPress}idName='four' buttonName='4' />
-          <Button buttonPress = {this.buttonPress}idName='five' buttonName='5' />
-          <Button buttonPress = {this.buttonPress}idName='six' buttonName='6' />
-          <Button buttonPress = {this.buttonPress}idName='seven' buttonName='7' />
-          <Button buttonPress = {this.buttonPress}idName='eight' buttonName='8' />
-          <Button buttonPress = {this.buttonPress}idName='nine' buttonName='9' />
-          <Button buttonPress = {this.buttonPress}idName='zero' buttonName='0' />
-          <Button buttonPress = {this.buttonPress}idName='decimal' buttonName='.' />
-          <Button buttonPress = {this.buttonPress}idName='clear' buttonName='AC' />
-          <Button buttonPress = {this.buttonPress}idName='equals' buttonName='=' /> 
+          <Button buttonPress = {this.displayOperator}idName='add' buttonName='+' />
+          <Button buttonPress = {this.displayOperator}idName='subtract' buttonName='-' />
+          <Button buttonPress = {this.displayOperator}idName='multiply' buttonName = '*'/>
+          <Button buttonPress = {this.displayOperator}idName='divide' buttonName='/' />
+          <Button buttonPress = {this.displayNumber} idName='one' buttonName='1' />
+          <Button buttonPress = {this.displayNumber} idName='two' buttonName='2' />
+          <Button buttonPress = {this.displayNumber} idName='three' buttonName='3' />
+          <Button buttonPress = {this.displayNumber} idName='four' buttonName='4' />
+          <Button buttonPress = {this.displayNumber} idName='five' buttonName='5' />
+          <Button buttonPress = {this.displayNumber} idName='six' buttonName='6' />
+          <Button buttonPress = {this.displayNumber} idName='seven' buttonName='7' />
+          <Button buttonPress = {this.displayNumber} idName='eight' buttonName='8' />
+          <Button buttonPress = {this.displayNumber} idName='nine' buttonName='9' />
+          <Button buttonPress = {this.displayNumber} idName='zero' buttonName='0' />
+          <Button buttonPress = {this.displayDecimal} idName='decimal' buttonName='.' />
+          <Button buttonPress = {this.clearCalculator} idName='clear' buttonName='AC' />
+          <Button buttonPress = {this.equateSum} idName='equals' buttonName='=' /> 
         </div>
       </div>
     );
